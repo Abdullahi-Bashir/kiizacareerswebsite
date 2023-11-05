760307edac9f951a96ba2dac2a47b80713a7246e
Abdullahi-Bashir <106074832+Abdullahi-Bashir@users.noreply.github.com>
2023-11-05 07:24:27 +0000

    add css file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, kiiza!'
+    return render_template('home.html')
+  # return send_from_directory('home.html', 'styles.css')
 
 
 if __name__ == '__main__':
